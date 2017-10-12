fdb0fee3893980e7ce902b87c566d6897ec7fbc8
Etherless <pabloimrik17@gmail.com>
2017-10-12 20:34:35 +0200

    Express setup and nodemon

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-var http = require("http");
+// var http = require("http");
+//
+// function handleRequests(req, res) {
+//     res.end('Hello World!');
+// }
+//
+// var server = http.createServer(handleRequests);
+//
+// server.listen(8080, function() {
+//     console.log('Listening on port 8080');
+// });
 
-function handleRequests(req, res) {
-    res.end('Hello World!');
-}
+// EXPRESS VERSION
 
-var server = http.createServer(handleRequests);
+var express = require('express');
 
-server.listen(8080, function() {
-    console.log('Listening on port 8080');
+var app = express();
+var port = 8080;
+
+app.listen(port, function() {
+    console.log('App started');
+});
+
+app.get('/', function(req, res) {
+   res.send('Hello World!');
 });
